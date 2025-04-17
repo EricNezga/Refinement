e52e69413169d05c1d5c54ca332d042e617e505c
EricNezga <96071946+EricNezga@users.noreply.github.com>
2025-04-17 14:40:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ function select(element, value) {
   const cards = container.querySelectorAll(".card-row");
   cards.forEach(card => card.classList.remove("selected"));
   element.classList.add("selected");
+  element.setAttribute("data-value", value);
 
   const selectedValues = Array.from(document.querySelectorAll(".factor")).map(f => {
     const sel = f.querySelector(".selected");
     if (!sel) return 0;
-    return parseInt(sel.querySelector(".card-title").textContent.match(/\d+/)[0]);
+    return valueFromCard(sel);
   });
 
   const total = selectedValues.reduce((acc, val) => acc + val, 0);
@@ -17,15 +18,21 @@ function select(element, value) {
   let sp = "-";
   if (total >= 3 && total <= 4) sp = 3;
   else if (total === 5) sp = 5;
-  else if (total >= 6 && total <= 7) sp = 8;
-  else if (total === 8) sp = 13;
+  else if (total >= 6 ) sp = 8;
+  else if (total >= 7 && total <= 8) sp = 13;
   else if (total === 9) sp = 21;
 
   storyPointDisplay.textContent = sp;
 
-  // Mostrar burbuja en cada columna
   const factorName = container.dataset.factor.toLowerCase();
   const bubble = document.getElementById(`bubble-${factorName}`);
   bubble.textContent = value;
   bubble.classList.add("active");
+}
+
+function valueFromCard(card) {
+  if (card.innerText.includes("ajustes") || card.innerText.includes("definida") || card.innerText.includes("ejecutar")) return 1;
+  if (card.innerText.includes("negocio") || card.innerText.includes("criterio") || card.innerText.includes("manejables")) return 2;
+  if (card.innerText.includes("capas") || card.innerText.includes("inestable") || card.innerText.includes("releases")) return 3;
+  return 0;
 }
