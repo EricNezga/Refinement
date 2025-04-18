5b719b360b88ba299bc447dd3f6850338b81247e
EricNezga <96071946+EricNezga@users.noreply.github.com>
2025-04-18 19:47:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,22 @@ toggleBtn.addEventListener("click", () => {
     ? `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-closed-icon"><path d="m15 18-.722-3.25"/><path d="M2 8a10.645 10.645 0 0 0 20 0"/><path d="m20 15-1.726-2.05"/><path d="m4 15 1.726-2.05"/><path d="m9 18 .722-3.25"/></svg>`
     : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>`;
 });
+
+const wrapper = document.querySelector(".wrapper");
+const cardContents = document.querySelectorAll(".card-content");
+
+function toggleView() {
+  const isSimple = wrapper.classList.toggle("simple");
+
+  cardContents.forEach(content => {
+    if (isSimple) {
+      content.style.transitionDelay = "0s";
+      content.style.opacity = "0";
+      content.style.visibility = "hidden";
+    } else {
+      content.style.transitionDelay = "200ms";
+      content.style.opacity = "1";
+      content.style.visibility = "visible";
+    }
+  });
+}
